2a2644e8d411102845261a9c716e185709f7f4f9
Abinet508 <abinatmail@gmail.com>
2022-09-20 23:15:29 +0300

    updated 🚀🚀EmployeeManager/EmployeeManager/Models/Employee.cs🚀🚀

---
+++ EmployeeManager/EmployeeManager/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace EmployeeManager.Models
+{
+    public partial class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+    }
+}
